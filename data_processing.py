c1a79029d71f7da256ae33277817c39f15f01d65
Saheed Yusuf <yusufsaheedtaiwo@gmail.com>
2024-08-17 22:30:06 +0100

    mlflow autolog implementation

--- data_processing.py
+++ data_processing.py
@@ -15,12 +15,12 @@ def eval_metrics(actual, pred):
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
  
-def save_corr_map(data):
+def save_corr_map(data, name = "heat-map.png"):
     corr = data.corr()
     plt.figure(figsize=(40,20))
     plot = sn.heatmap(corr, annot=True, cmap='coolwarm')
     fig = plot.get_figure()
-    fig.savefig("heat-map.png")
+    fig.savefig(name)
     
 def split_data(data):
     train, test = train_test_split(data,random_state=42)
@@ -32,32 +32,39 @@ def split_data(data):
     
     return train_x, train_y, test_x, test_y
 
-def train_and_generate_linear_regression_model(train_x, train_y, test_x, test_y):
+def train_and_generate_linear_regression_model(train_x, train_y, test_x, test_y, model_name='fare_model_linear_regression.pkl'):
     lr = LinearRegression()
     lr.fit(train_x, train_y['fare_amount'].to_list())
-    joblib.dump(lr, 'fare_model_linear_regression.pkl')
+    joblib.dump(lr, model_name)
     predicted_qualities = lr.predict(test_x)
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
     return rmse, mae, r2
 
 
-def train_and_generate_decision_tree_regressor_model(train_x, train_y, test_x, test_y):
+def train_and_generate_decision_tree_regressor_model(train_x, train_y, test_x, test_y, model_name='fare_model_decision_tree_regressor.pkl'):
     decision_tree_model = DecisionTreeRegressor()
     decision_tree_model.fit(train_x, train_y)
-    joblib.dump(decision_tree_model, 'fare_model_decision_tree_regressor.pkl')
+    joblib.dump(decision_tree_model,model_name)
     predicted_qualities2 = decision_tree_model.predict(test_x)
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities2)
     return rmse, mae, r2
 
-def process_data(data):
+def drop_unused_column_split_data(data):
+    selected_df = data.drop(columns=['payment_type_4', 'payment_type_2','improvement_surcharge','congestion_surcharge'])
+    selected_df.drop(columns=['airport_fee'], inplace=True)
+        
+    return split_data(selected_df)
+
+
+def process_data_and_return_train_and_test_data(data):
     data = clean_data(data)
     
     save_corr_map(data)
     
-    selected_df = data.drop(columns=['payment_type_4', 'payment_type_2','improvement_surcharge','congestion_surcharge'])
-    selected_df.drop(columns=['airport_fee'], inplace=True)
-        
-    train_x, train_y, test_x, test_y = split_data(selected_df)
+    train_x, train_y, test_x, test_y = drop_unused_column_split_data(data)
     
     print(train_and_generate_linear_regression_model(train_x, train_y, test_x, test_y))
     print(train_and_generate_decision_tree_regressor_model(train_x, train_y, test_x, test_y))
+    
+    return train_x, train_y, test_x, test_y
+
--- main.py
+++ main.py
@@ -3,8 +3,34 @@ import pandas as pd
 import glob
 import os
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from data_processing import process_data
+from clean_data import clean_data
+from data_processing import drop_unused_column_split_data, process_data_and_return_train_and_test_data, save_corr_map, train_and_generate_decision_tree_regressor_model, train_and_generate_linear_regression_model
 
-data = pd.concat(map(pd.read_parquet, glob.glob(os.path.join('', "/Users/saheedyusuf/Downloads/yellow-taxi-files/yellow_trip*.parquet"))))
-data.ffill(inplace=True)
-process_data(data=data)
+raw_data = pd.concat(map(pd.read_parquet, glob.glob(os.path.join('', "/Users/saheedyusuf/Downloads/yellow-taxi-files/yellow_trip*.parquet"))))
+raw_data.ffill(inplace=True)
+
+experiment = mlflow.get_experiment_by_name('nyc-taxi')
+# if (experiment.)
+
+with mlflow.start_run(experiment_id=experiment.experiment_id):
+    mlflow.autolog()
+    data = clean_data(raw_data)
+    save_corr_map(data)
+    
+    train_x, train_y, test_x, test_y = drop_unused_column_split_data(data)
+    train_and_generate_linear_regression_model(train_x, train_y, test_x, test_y, model_name='ml-flow-trained-model-linear-regression.pkl')
+    # train_and_generate_decision_tree_regressor_model(train_x, train_y, test_x, test_y, model_name='ml-flow-trained-model-decision-tree-regressor.pkl')
+    
+
+with mlflow.start_run(experiment_id=experiment.experiment_id):
+    mlflow.autolog()
+    data = clean_data(raw_data)
+    save_corr_map(data)
+    
+    train_x, train_y, test_x, test_y = drop_unused_column_split_data(data)
+    # train_and_generate_linear_regression_model(train_x, train_y, test_x, test_y, model_name='ml-flow-trained-model-linear-regression.pkl')
+    train_and_generate_decision_tree_regressor_model(train_x, train_y, test_x, test_y, model_name='ml-flow-trained-model-decision-tree-regressor.pkl')
+
+# process_data_and_return_train_and_test_data(data=data)
+
+
