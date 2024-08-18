798980a7329320782e02a166f03070fe1c57a0d5
Saheed Yusuf <yusufsaheedtaiwo@gmail.com>
2024-08-18 11:48:20 +0100

    added script to create manual model

--- main-manul-model-log.py
+++ main-manul-model-log.py
@@ -2,6 +2,7 @@ import mlflow
 import pandas as pd
 import glob
 import os
+import joblib
 from clean_data import clean_data
 from data_processing import drop_unused_column_split_data, save_corr_map, train_and_generate_decision_tree_regressor_model, train_and_generate_linear_regression_model
 
@@ -11,21 +12,24 @@ raw_data.ffill(inplace=True)
 experiment = mlflow.get_experiment_by_name('nyc-taxi')
 
 with mlflow.start_run(experiment_id=experiment.experiment_id):
-    mlflow.autolog()
+    mlflow.autolog(log_models=False)
     data = clean_data(raw_data)
-    save_corr_map(data)
+    save_corr_map(data, 'lr-heat-map.png')
     
     train_x, train_y, test_x, test_y = drop_unused_column_split_data(data)
     train_and_generate_linear_regression_model(train_x, train_y, test_x, test_y, model_name='ml-flow-trained-model-linear-regression.pkl')
+    mlflow.sklearn.log_model(joblib.load('ml-flow-trained-model-linear-regression.pkl'), 'model')
+    mlflow.log_artifact('lr-heat-map.png')
     
 
 with mlflow.start_run(experiment_id=experiment.experiment_id):
-    mlflow.autolog()
+    mlflow.autolog(log_models=False)
     data = clean_data(raw_data)
-    save_corr_map(data)
+    save_corr_map(data, 'dtr-heat-map.png')
     
     train_x, train_y, test_x, test_y = drop_unused_column_split_data(data)
     train_and_generate_decision_tree_regressor_model(train_x, train_y, test_x, test_y, model_name='ml-flow-trained-model-decision-tree-regressor.pkl')
-
+    mlflow.sklearn.log_model(joblib.load('ml-flow-trained-model-decision-tree-regressor.pkl'), 'model')
+    mlflow.log_artifact('dtr-heat-map.png')
 
 
